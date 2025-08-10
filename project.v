04024708c5e2f46680d422895a93a215a9ba2386
uttamchirukuri <uttamchirukuri2531@gmail.com>
2025-08-10 12:23:40 +0530

    Update project.v

--- project.v
+++ project.v
@@ -7,7 +7,7 @@
 
 
 
-module tt_um_secure_serdes_encryptor (
+module tt_um_secure_serdes_encrypt (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
